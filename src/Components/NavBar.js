6188ba7d380dae97d8c7159ec18e003b8fa5eccd
leaskn <leask.nicholas@gmail.com>
2021-11-16 16:18:48 -0500

    updated icons

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,6 @@
-import react from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 export default function(){
   return(
@@ -7,7 +9,7 @@ export default function(){
 
         <li class="logo">
           <a href="#" class="nav-link">
-            <span class="link-text logo-text top-logo">Nic<br/>Leask</span>
+            <span className="link-text logo-text top-logo">Nic<br/>Leask</span>
             <svg
               aria-hidden="true"
               focusable="false"
@@ -36,117 +38,55 @@ export default function(){
 
         <li class="nav-item">
           <a href="#" class="nav-link">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fad"
-              data-icon="cat"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              class="svg-inline--fa fa-cat fa-w-16 fa-9x"
-            >
-              <g class="fa-group">
-                <path
-                  fill="currentColor"
-                  d="M448 96h-64l-64-64v134.4a96 96 0 0 0 192 0V32zm-72 80a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm80 0a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm-165.41 16a204.07 204.07 0 0 0-34.59 2.89V272l-43.15-64.73a183.93 183.93 0 0 0-44.37 26.17L192 304l-60.94-30.47L128 272v-80a96.1 96.1 0 0 0-96-96 32 32 0 0 0 0 64 32 32 0 0 1 32 32v256a64.06 64.06 0 0 0 64 64h176a16 16 0 0 0 16-16v-16a32 32 0 0 0-32-32h-32l128-96v144a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V289.86a126.78 126.78 0 0 1-32 4.54c-61.81 0-113.52-44.05-125.41-102.4z"
-                  class="fa-secondary"
-                ></path>
-                <path
-                  fill="currentColor"
-                  d="M376 144a16 16 0 1 0 16 16 16 16 0 0 0-16-16zm80 0a16 16 0 1 0 16 16 16 16 0 0 0-16-16zM131.06 273.53L192 304l-23.52-70.56a192.06 192.06 0 0 0-37.42 40.09zM256 272v-77.11a198.62 198.62 0 0 0-43.15 12.38z"
-                  class="fa-primary"
-                ></path>
-              </g>
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24">
+              <path className="fa-primary" d="M19,11v9h-5v-6h-4v6H5v-9H3.6L12,3.4l8.4,7.6H19z" opacity=".3"/>
+              <path className="fa-secondary" d="M20,21h-7v-6h-2v6H4v-9H1l11-9.9L23,12h-3V21z M15,19h3v-8.8l-6-5.4l-6,5.4V19h3v-6h6V19z"/>
             </svg>
-            <span class="link-text">Home</span>
+            Home
           </a>
         </li>
 
         <li class="nav-item">
           <a href="#" class="nav-link">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fad"
-              data-icon="alien-monster"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 576 512"
-              class="svg-inline--fa fa-alien-monster fa-w-18 fa-9x"
-            >
-              <g class="fa-group">
-                <path
-                  fill="currentColor"
-                  d="M560,128H528a15.99954,15.99954,0,0,0-16,16v80H480V176a15.99954,15.99954,0,0,0-16-16H416V96h48a16.00079,16.00079,0,0,0,16-16V48a15.99954,15.99954,0,0,0-16-16H432a15.99954,15.99954,0,0,0-16,16V64H368a15.99954,15.99954,0,0,0-16,16v48H224V80a15.99954,15.99954,0,0,0-16-16H160V48a15.99954,15.99954,0,0,0-16-16H112A15.99954,15.99954,0,0,0,96,48V80a16.00079,16.00079,0,0,0,16,16h48v64H112a15.99954,15.99954,0,0,0-16,16v48H64V144a15.99954,15.99954,0,0,0-16-16H16A15.99954,15.99954,0,0,0,0,144V272a16.00079,16.00079,0,0,0,16,16H64v80a16.00079,16.00079,0,0,0,16,16h48v80a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V432a15.99954,15.99954,0,0,0-16-16H192V384H384v32H336a15.99954,15.99954,0,0,0-16,16v32a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V384h48a16.00079,16.00079,0,0,0,16-16V288h48a16.00079,16.00079,0,0,0,16-16V144A15.99954,15.99954,0,0,0,560,128ZM224,320H160V224h64Zm192,0H352V224h64Z"
-                  class="fa-secondary"
-                ></path>
-                <path
-                  fill="currentColor"
-                  d="M160,320h64V224H160Zm192-96v96h64V224Z"
-                  class="fa-primary"
-                ></path>
-              </g>
-            </svg>
-            <span class="link-text">About</span>
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">
+              <path className="fa-primary" d="M20,11c0-4.4-3.6-8-8-8s-8,3.6-8,8s3.6,8,8,8c0.3,0,0.7,0,1-0.1v2.3c2.1-1.4,6-4.4,6.8-8.6C19.9,12.1,20,11.6,20,11z" opacity=".3"/>
+              <path className="fa-secondary" d="M12,23.1V20c-5,0-9-4-9-9c0-5,4-9,9-9c5,0,9,4,9,9c0,0.6-0.1,1.3-0.2,1.9c-0.9,4.5-4.9,7.7-7.3,9.2L12,23.1z M14,17.8v1.5 c2-1.5,4.2-3.9,4.8-6.8c0.1-0.5,0.2-1,0.2-1.5c0-3.9-3.1-7-7-7c-3.9,0-7,3.1-7,7s3.1,7,7,7c0.3,0,0.6,0,0.9-0.1L14,17.8z"/></svg>
+            <span className="link-text">About</span>
           </a>
         </li>
 
         <li class="nav-item">
           <a href="#" class="nav-link">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fad"
-              data-icon="space-station-moon-alt"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              class="svg-inline--fa fa-space-station-moon-alt fa-w-16 fa-5x"
-            >
-              <g class="fa-group">
-                <path
-                  fill="currentColor"
-                  d="M501.70312,224H448V160H368V96h48V66.67383A246.86934,246.86934,0,0,0,256,8C119.03125,8,8,119.0332,8,256a250.017,250.017,0,0,0,1.72656,28.26562C81.19531,306.76953,165.47656,320,256,320s174.80469-13.23047,246.27344-35.73438A250.017,250.017,0,0,0,504,256,248.44936,248.44936,0,0,0,501.70312,224ZM192,240a80,80,0,1,1,80-80A80.00021,80.00021,0,0,1,192,240ZM384,343.13867A940.33806,940.33806,0,0,1,256,352c-87.34375,0-168.71094-11.46094-239.28906-31.73633C45.05859,426.01953,141.29688,504,256,504a247.45808,247.45808,0,0,0,192-91.0918V384H384Z"
-                  class="fa-secondary"
-                ></path>
-                <path
-                  fill="currentColor"
-                  d="M256,320c-90.52344,0-174.80469-13.23047-246.27344-35.73438a246.11376,246.11376,0,0,0,6.98438,35.998C87.28906,340.53906,168.65625,352,256,352s168.71094-11.46094,239.28906-31.73633a246.11376,246.11376,0,0,0,6.98438-35.998C430.80469,306.76953,346.52344,320,256,320Zm-64-80a80,80,0,1,0-80-80A80.00021,80.00021,0,0,0,192,240Zm0-104a24,24,0,1,1-24,24A23.99993,23.99993,0,0,1,192,136Z"
-                  class="fa-primary"
-                ></path>
-              </g>
-            </svg>
-            <span class="link-text">Projects</span>
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">
+              <path className='fa-primary' d="M20,19H4c-0.6,0-1-0.4-1-1V6c0-0.6,0.4-1,1-1h16c0.6,0,1,0.4,1,1v12C21,18.6,20.6,19,20,19z" opacity=".3"/>
+              <path className='fa-secondary' d="M9 3H15V5H9z"/>
+              <path className='fa-secondary' d="M20,20H4c-1.1,0-2-0.9-2-2V6c0-1.1,0.9-2,2-2h16c1.1,0,2,0.9,2,2v12C22,19.1,21.1,20,20,20z M20,18v1V18L20,18z M4,6v12h16 l0-12H4z"/>
+              <path className='fa-secondary' d="M12 10A1 1 0 1 0 12 12A1 1 0 1 0 12 10Z"/>
+              <path className='fa-secondary' d="M20,16H4c-1.1,0-2-0.9-2-2v-4h2v4h16v-4h2v4C22,15.1,21.1,16,20,16z"/></svg>
+            <span className="link-text">Projects</span>
           </a>
         </li>
 
         <li class="nav-item">
           <a href="#" class="nav-link">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fad"
-              data-icon="space-shuttle"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 640 512"
-              class="svg-inline--fa fa-space-shuttle fa-w-20 fa-5x"
-            >
-              <g class="fa-group">
-                <path
-                  fill="currentColor"
-                  d="M32 416c0 35.35 21.49 64 48 64h16V352H32zm154.54-232h280.13L376 168C243 140.59 222.45 51.22 128 34.65V160h18.34a45.62 45.62 0 0 1 40.2 24zM32 96v64h64V32H80c-26.51 0-48 28.65-48 64zm114.34 256H128v125.35C222.45 460.78 243 371.41 376 344l90.67-16H186.54a45.62 45.62 0 0 1-40.2 24z"
-                  class="fa-secondary"
-                ></path>
-                <path
-                  fill="currentColor"
-                  d="M592.6 208.24C559.73 192.84 515.78 184 472 184H186.54a45.62 45.62 0 0 0-40.2-24H32c-23.2 0-32 10-32 24v144c0 14 8.82 24 32 24h114.34a45.62 45.62 0 0 0 40.2-24H472c43.78 0 87.73-8.84 120.6-24.24C622.28 289.84 640 272 640 256s-17.72-33.84-47.4-47.76zM488 296a8 8 0 0 1-8-8v-64a8 8 0 0 1 8-8c31.91 0 31.94 80 0 80z"
-                  class="fa-primary"
-                ></path>
-              </g>
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">
+              <path className="fa-primary" d="M19,20c0,0.6-0.4,1-1,1H6c-0.6,0-1-0.4-1-1V4c0-0.6,0.4-1,1-1h7.6L19,8.4V20z" opacity=".3"/>
+              <path className="fa-primary" d="M8 12H16V14H8zM8 16H16V18H8z"/>
+              <path className="fa-secondary" d="M18,22H6c-1.1,0-2-0.9-2-2V4c0-1.1,0.9-2,2-2h8l6,6v12C20,21.1,19.1,22,18,22z M6,4v16h12V8.8L13.2,4H6z"/>
+              <path className="fa-primary" d="M18.5 9L13 9 13 3.5z"/></svg>
+            <span className="link-text">Resume</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">
+              <path className="fa-secondary" d="M7,5C4.8,5,3,6.8,3,9v9c0,0.6,0.4,1,1,1h16c0.6,0,1-0.4,1-1V9c0-2.2-1.8-4-4-4H7z" opacity=".3"/>
+              <path className="fa-secondary" d="M20,20H4c-1.1,0-2-0.9-2-2V9c0-2.8,2.2-5,5-5h10c2.8,0,5,2.2,5,5v9C22,19.1,21.1,20,20,20z M7,6C5.3,6,4,7.3,4,9v9h16V9 c0-1.7-1.3-3-3-3H7z"/>
+              <path className="fa-secondary" d="M12,19h-2V9c0-1.7-1.3-3-3-3V4c2.8,0,5,2.2,5,5V19z"/>
+              <path className="fa-primary" d="M6 9H8V11H6zM18 13L16 13 16 11 11 11 11 9 18 9z"/>
             </svg>
-            <span class="link-text">Resume</span>
+            Contact
           </a>
         </li>
 
@@ -163,12 +103,12 @@ export default function(){
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
               class="svg-inline--fa fa-moon-stars fa-w-16 fa-7x"
-            >
+              >
               <g class="fa-group">
-                <path
-                  fill="currentColor"
-                  d="M320 32L304 0l-16 32-32 16 32 16 16 32 16-32 32-16zm138.7 149.3L432 128l-26.7 53.3L352 208l53.3 26.7L432 288l26.7-53.3L512 208z"
-                  class="fa-secondary"
+              <path
+              fill="currentColor"
+              d="M320 32L304 0l-16 32-32 16 32 16 16 32 16-32 32-16zm138.7 149.3L432 128l-26.7 53.3L352 208l53.3 26.7L432 288l26.7-53.3L512 208z"
+              class="fa-secondary"
                 ></path>
                 <path
                   fill="currentColor"
@@ -202,7 +142,7 @@ export default function(){
                 ></path>
               </g>
             </svg>
-            <span class="link-text">Dark-Light</span>
+            <span className="link-text">Dark-Light</span>
           </a>
         </li>
       </ul>
