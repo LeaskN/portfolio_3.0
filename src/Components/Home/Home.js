5f4232f4a8fe3812dd69d41f71475d153accf6eb
leaskn <leask.nicholas@gmail.com>
2022-01-14 10:57:04 -0500

    updated colors

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,8 @@ import './Home.css';
 export default function Home(props){
   return(
     <div className="home" ref={props.refProp}>
-      <h1 className="intro">Hi, I'm Nic</h1>
+      <h1 className="intro">Hi, I'm <span className='colored'>Nic</span></h1>
+      <h3>I'm a <span className='colored'>software engineer</span></h3>
     </div>
   )
 }
--- src/Components/Scroller/Scroller.js
+++ src/Components/Scroller/Scroller.js
@@ -4,7 +4,7 @@ import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai';
 
 export default function Scroller(props){
   const scroll = (e) => {
-    console.log(window.scrollY, props.scrollOptions.reduce((a, c) => a+c,0))
+    if(!props.scrollOptions) return
     if(window.scrollY <= props?.scrollOptions[1]-20){
       props?.scrollTo.scrollToAbout(e);
     } else if(window.scrollY <= props?.scrollOptions[1] + props?.scrollOptions[2]-20){
@@ -21,7 +21,7 @@ export default function Scroller(props){
   if(props.bottom){
     return(
       <div className='scroller' onClick={(e) => scroll(e)} >
-        { <AiOutlineUp /> }
+        { <AiOutlineDown className='flipped' /> }
       </div>
     )
   } else {
