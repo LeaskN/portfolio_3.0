192cc6fdd25a5dc32e209dc1487c421c55c3e064
leaskn <leask.nicholas@gmail.com>
2022-01-13 21:18:00 -0500

    added scrolling to sections

--- src/Components/Scroller/Scroller.js
+++ src/Components/Scroller/Scroller.js
@@ -3,31 +3,22 @@ import './Scroller.css';
 import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai';
 
 export default function Scroller(props){
-  const [ location, setLocation ] = useState(0);
-
- 
-
   const scroll = (e) => {
-    console.log(location)
-    if(location === 'home'){
-      setLocation('about');
-      console.log(props)
-      props.scrollTo.scrollToAbout(e);
-    } else if(location === 'about'){
-      setLocation('port');
-      props.scrollTo.scrollToPort(e);
-    } else if(location === 'port'){
-      setLocation('contact');
-      props.scrollTo.scrollToContact(e);
-    } else {
-      setLocation('home');
-      props.scrollTo.scrollToHome(e);
+    console.log(window.scrollY, props.scrollOptions.reduce((a, c) => a+c,0))
+    if(window.scrollY <= props?.scrollOptions[1]-20){
+      props?.scrollTo.scrollToAbout(e);
+    } else if(window.scrollY <= props?.scrollOptions[1] + props?.scrollOptions[2]-20){
+      props?.scrollTo.scrollToPort(e);
+    } else if(window.scrollY <= props?.scrollOptions[1] + props?.scrollOptions[2] + props?.scrollOptions[3]-20){
+      props?.scrollTo.scrollToContact(e);
+    } else if (window.scrollY >= props?.scrollOptions[1] + props?.scrollOptions[2] + props?.scrollOptions[3]-20){
+      props?.scrollTo.scrollToHome(e);
     }
   }
 
   
 
-  if(location === 'contact' || props.bottom){
+  if(props.bottom){
     return(
       <div className='scroller' onClick={(e) => scroll(e)} >
         { <AiOutlineUp /> }
