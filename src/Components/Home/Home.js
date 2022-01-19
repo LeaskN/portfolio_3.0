d6d79b89bfbcf15a6ae21342f4c48cfb4235b0f5
Nicholas Leask <leask.nicholas@gmail.com>
2022-01-19 11:04:36 -0500

    updated icon

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 export default function Home(props){
   return(
     <div className="home" ref={props.refProp}>
-      <h1 className="intro">Hi, I'm <span className='colored'>Nic</span></h1>
+      <h1 className="intro">Hi, I'm <span className='colored'><span className='letterIcon'>N</span>ic</span></h1>
       <h3>I'm a <span className='colored'>software engineer</span></h3>
     </div>
   )
