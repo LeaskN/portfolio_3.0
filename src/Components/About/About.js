e36c17c12d5253080c80fa4fa9277706304ac0d9
leaskn <leask.nicholas@gmail.com>
2022-01-20 12:44:50 -0500

    feat: pages are flex-column & added about header

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function About(props){
   return(
     <div>
-      <div className='page' id='about' ref={props.refProp}>Professionally, I am a software engineer with experience in full stack web development. I'm currently working with React, Javascript, Node.js, HTML, and CSS. I'm consistently working on new projects. Most recently these projects have been revolving around React, however I am always working to improve my software engineering skills</div>
+      
+      <div className='page' id='about' ref={props.refProp}>
+        <h1>About me</h1>
+        <p>Professionally, I am a software engineer with experience in full stack web development. I'm currently working with React, Javascript, Node.js, HTML, and CSS. I'm consistently working on new projects. Most recently these projects have been revolving around React, however I am always working to improve my software engineering skills</p>
+      </div>
     </div>
   ) 
 }
