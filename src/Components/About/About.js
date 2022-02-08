4b39ef6552add8d77fdafbd5041a239a9be49e2a
leaskn <leask.nicholas@gmail.com>
2022-02-07 19:31:43 -0500

    style:updated about text

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import './About.css';
 
 export default function About(props){
   return(
     <div>
-      
       <div className='page' id='about' ref={props.refProp}>
-        <h1>About me</h1>
-        <p>Professionally, I am a software engineer with experience in full stack web development. I'm currently working with React, Javascript, Node.js, HTML, and CSS. I'm consistently working on new projects. Most recently these projects have been revolving around React, however I am always working to improve my software engineering skills</p>
+        <h1 className='colored'>About me</h1>
+        <h3>Professionally, I am a <span className='highlightedText'>software engineer</span> with experience in <span className='highlightedText'>full stack web development</span>. I'm currently working with <span className='highlightedText'>React</span>, <span className='highlightedText'>Javascript</span>, <span className='highlightedText'>Node.js</span>, <span className='highlightedText'>HTML</span>, <span className='highlightedText'>CSS</span>, <span className='highlightedText'>AWS</span>, <span className='highlightedText'>Salesforce</span>, and more! I'm consistently working on new projects. Most recently these projects have been revolving around <span className='highlightedText'>React</span>, however I am always open to new tech and love learning new things.</h3>
       </div>
     </div>
   ) 
